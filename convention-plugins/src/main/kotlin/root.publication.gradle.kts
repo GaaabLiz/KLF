69e085a374cade381b08b53fe6fbe73491b1d30f
GaaabLiz <lizzosgabriele@gmail.com>
2024-03-08 20:39:22 +0100

    added ossrh credential in nexus

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -14,6 +14,8 @@ nexusPublishing {
         sonatype {  //only for users registered in Sonatype after 24 Feb 2021
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("OSSRH_USERNAME") as? String ?: "unset")
+            password.set(System.getenv("OSSRH_PASSWORD") as? String ?: "unset")
         }
     }
 }
